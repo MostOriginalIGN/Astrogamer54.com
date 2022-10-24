6478b47c03f914dafa59f7e77d0732d83458c56b
Astrogamer54 <78519393+Astrogamer54@users.noreply.github.com>
2022-10-23 22:34:50 -0700

    Fix Mobile Landing Screen Bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function() {
     const isSmall = window.matchMedia('only screen and (max-width: 800px)').matches;
     const isVertical = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
     const mobileAgent = typeof window.orientation !== 'undefined'
@@ -6,6 +6,7 @@ window.onload = function () {
     // Doesn't work on iOS Chrome (based on tests)
     console.log(isMobile);
     if (isMobile) {
+        document.documentElement.style.setProperty("--background-color", '#43e');
         var content = document.getElementById('content');
         content.remove();
         var content = document.getElementById('nav-menu');
@@ -15,10 +16,10 @@ window.onload = function () {
         mobileMain.innerHTML = "<div class='mobile-div glow-section' id='mobile-div'><a href='#'>Astrogamer54.com</a><h1>we ran into a problem :(</h1><h1>your screensize is too small for this website</h1></div>";
         document.body.appendChild(mobileMain);
         document.body.id = "nav-menu"; // scuffed work around
-    } 
+    }
     // WHY IS THIS NOT WORKING PLS HELP
     var prevScrollpos = window.pageYOffset;
-    document.body.onscroll = function () {
+    document.body.onscroll = function() {
         console.log("a");
         var currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
@@ -40,29 +41,55 @@ window.onload = function () {
             nav.style.setProperty("--mouse-x", `${x}px`);
             nav.style.setProperty("--mouse-y", `${y}px`);
         };
-
     };
     document.getElementById('nav-menu').onmouseenter = function fade(e) {
-        var max = 100;
-        for (const nav of document.getElementsByClassName("glow-section")) {
-            if (!effectcheck) {
-                effectcheck = setInterval(repeatcheck, 1);
-            }
-            function repeatcheck() {
-                if (max > 0) {
-                    max = max - 1
-                } else {
-                    clearInterval(effectcheck);
+        if (!isMobile) {
+            var max = 100;
+            for (const nav of document.getElementsByClassName("glow-section")) {
+                if (!effectcheck) {
+                    effectcheck = setInterval(repeatcheck, 1);
                 }
-                document.documentElement.style.setProperty("--glow-percent", `${100 - max}%`);
 
+                function repeatcheck() {
+                    if (max > 0) {
+                        max = max - 1
+                    } else {
+                        clearInterval(effectcheck);
+                    }
+                    document.documentElement.style.setProperty("--glow-percent", `${100 - max}%`);
+
+                };
             };
-        };
+        }
+
     };
-    document.getElementById('nav-menu').onmouseleave = function fade(e) {
+    document.getElementById('nav-menu').onmouseleave = function fadebye(e) {
         clearInterval(effectcheck);
         var max = 100;
         effectcheck = null;
     };
-    document.addEventListener('contextmenu', event => event.preventDefault());
+    document.getElementById('nav-menu').onclick = function fadee(e) {
+        let effectcheck;
+        if (isMobile) {
+            var max = 100;
+            for (const nav of document.getElementsByClassName("glow-section")) {
+                if (!effectcheck) {
+                    effectcheck = setInterval(repeatcheck, 1);
+                };
+
+                function repeatcheck() {
+                    if (!(max == 0)) {
+                        max = max - 2;
+                        console.log(max);
+                    } else {
+                        console.log(max);
+                        clearInterval(effectcheck);
+                    };
+                    document.documentElement.style.setProperty("--glow-percent", `${100 - max}%`);
+
+                };
+            };
+        };
+        document.addEventListener('contextmenu', event => event.preventDefault());
+    };
 };
