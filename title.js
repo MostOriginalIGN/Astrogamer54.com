41675b363bdf6b0e06f12beb44a5246df847a760
Astrogamer54 <78519393+Astrogamer54@users.noreply.github.com>
2022-11-26 14:33:48 -0800

    basic trash footer

---
+++ title.js
@@ -0,0 +1,19 @@
+const subtitle = document.getElementsByClassName("title")[0];
+
+const createWord = (text, index) => {
+  const word = document.createElement("span");
+  
+  word.innerHTML = `${text} `;
+  
+  word.classList.add("title-word");
+  
+  word.style.transitionDelay = `${index * 40}ms`;
+  
+  return word;
+}
+
+const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
+
+const createSubtitle = text => text.split(" ").map(addWord);
+
+createSubtitle("But in a much more real sense, I have no idea what I'm doing.");
